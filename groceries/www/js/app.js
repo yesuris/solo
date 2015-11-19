20b14e7a78f13fbe7ad2a69e9cad2f52d481c62a
yesuris <yecaesra@hotmail.com>
2015-11-19 12:58:58 -0500

    porfin logre guardad en una base de datos de firebase, el error que tenia es que de loca cuando estab conociendo me puse a agregarle una regla y apenas se la quite y le deje las que vienen por defecto funciono todo :) :) be happy don´t worry

--- groceries/www/js/app.js
+++ groceries/www/js/app.js
@@ -3,24 +3,29 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
-.controller('ListCtrl', function($scope, $ionicListDelegate) {
+.factory('Items', ['$firebaseArray', function($firebaseArray) {
+  var itemsRef = new Firebase('https://chatbdurl.firebaseio.com/items');
+  return $firebaseArray(itemsRef);
+}])
 
-  $scope.items = [];
+.controller('ListCtrl', function($scope, $ionicListDelegate, Items) {
+
+  $scope.items = Items;
 
   $scope.addItem = function() {
     var name = prompt('What do you need to buy?');
     if (name) {
-      $scope.items.push({
+      $scope.items.$add({
         'name': name
       });
     }
   };
 
   $scope.purchaseItem = function(item) {
-  	$scope.item = item;
-  	$scope.item['status'] = 'purchased';
+  	var itemRef = new Firebase('https://chatbdurl.firebaseio.com/items/' + item.$id);
+  	itemRef.child('status').set('purchased');
   	$ionicListDelegate.closeOptionButtons();
   };
 });
